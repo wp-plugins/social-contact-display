38531a3cc09897a6a84a52a2d72ff9c97c3f61cc
hptonline <hptonline@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-17 16:31:15 +0000

    Minor update
    git-svn-id: https://plugins.svn.wordpress.org/social-contact-display/trunk@805805 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/css1.php
+++ includes/css1.php
@@ -44,26 +44,26 @@
    }
    // Check if facebook is set
    if( $facebook ) {
-    echo '<div class="hpt-social-facebook"><a href="'.$facebook.'" target="_blank" class="social"></a>&nbsp;</div>';
+    echo '<div class="hpt-social-facebook"><a href="'.$facebook.'" target="_blank" class="social"></a></div>&nbsp;';
    }
    // Check if twitter is set
   if( $twitter ) {
-     echo '<div class="hpt-social-twitter"><a href="'.$twitter.'" target="_blank" class="social"></a>&nbsp;</div>';
+     echo '<div class="hpt-social-twitter"><a href="'.$twitter.'" target="_blank" class="social"></a></div>&nbsp;';
    }
    // Check if gplus is set
    if( $gplus ) {
-     echo '<div class="hpt-social-googleplus"><a href="'.$gplus.'" target="_blank" class="social"></a>&nbsp;</div>';
+     echo '<div class="hpt-social-googleplus"><a href="'.$gplus.'" target="_blank" class="social"></a></div>&nbsp;';
    }
    // Check if linkedin is set
    if( $linkedin ) {
-     echo '<div class="hpt-social-linkedin"><a href="'.$linkedin.'" target="_blank" class="social"></a>&nbsp;</div>';
+     echo '<div class="hpt-social-linkedin"><a href="'.$linkedin.'" target="_blank" class="social"></a></div>&nbsp;';
    }
    // Check if youtube is set
    if( $youtube ) {
-     echo '<div class="hpt-social-youtube"><a href="'.$youtube.'" target="_blank" class="social"></a>&nbsp;</div>';
+     echo '<div class="hpt-social-youtube"><a href="'.$youtube.'" target="_blank" class="social"></a></div>&nbsp;';
    }
    // Check if pinterest is set
    if( $pinterest ) {
-     echo '<div class="hpt-social-pinterest"><a href="'.$pinterest.'" target="_blank" class="social"></a>&nbsp;</div>';
+     echo '<div class="hpt-social-pinterest"><a href="'.$pinterest.'" target="_blank" class="social"></a></div>&nbsp;';
    }   
 ?>
